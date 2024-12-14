cbff350cedf21ddefacb07bb26ac7d375f4166f9
丁少华 <dingshaohua@douyuxingchen.com>
2024-12-14 14:22:54 +0800

    Update vite.config.ts

--- src/apps/demo/vite.config.ts
+++ src/apps/demo/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/demo/",
+  base: "/demo",
   plugins: [react()],
   resolve: {
     alias: {
